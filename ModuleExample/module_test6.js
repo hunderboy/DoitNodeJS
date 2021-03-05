e5371e0d95ddc2dfd0f6f755fd997ad72906668c
hunderboy <wheeing@naver.com>
2021-03-05 14:53:00 +0900

    // prototype 적용 연습 중

--- ModuleExample/module_test6.js
+++ ModuleExample/module_test6.js
@@ -11,10 +11,10 @@
 // 가상으로 require() 함수를 정의해 보면 require() 함수가 내부적으로 처리되는 방식을 이해할 수 있음
 var require = function(path) {
 	var exports = {
-		getUser: function() {
+		getUser: function() {	// 속성에 함수 할당
 			return {id:'test01', name:'소녀시대'};
 		},
-		group:{id:'group01', name:'친구'}
+		group:{id:'group01', name:'친구'} // 속성에 객체 할당
 	}
 	
 	return exports;
--- ModuleExample/user7.js
+++ ModuleExample/user7.js
@@ -9,6 +9,6 @@
 
 // 사용 패턴 : exports에 속성으로 추가된 함수 객체를 그대로 참조한 후 호출함
 
-exports.printUser = function() {
+exports.printUser = function() { // 속성에 함수 할당
 	console.log('user 이름은 소녀시대입니다.');
 };
